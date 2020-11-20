2eec4d1ac018b426b4469f6b658e0341bb98ff81
stef <stef@thestandupway.org>
2020-11-20 11:59:37 +0100

    Updated exports

--- cockpit/production/production.jl
+++ cockpit/production/production.jl
@@ -7,6 +7,6 @@ export Lifecycle, SingleUse, Restorable
 export Identity
 export Entity, Product, Enhancer, Consumable, Tool, Producer
 export BluePrint, ConsumableBluePrint, ToolBluePrint, ProducerBluePrint
-export health, id, type_id, name_of, use!, produce!, do_maintenance!
+export health, id, type_id, name_of, use!, produce!, restore!
 
 end
