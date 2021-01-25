27fc10e8d3a4a349f0a80adfca8719392ecb4bf4
stef <stef@happonomy.org>
2021-01-25 15:39:39 +0100

    Renamed file to avoid name clash with Agents module.

--- cockpit/abm/abm.jl
+++
@@ -1,3 +0,0 @@
-module ABM
-    include("model.jl")
-end
---
+++ cockpit/abm/abm_sim.jl
@@ -0,0 +1,4 @@
+module ABM_Sim
+    include("model.jl")
+    export Person, get_production, get_needs, get_posessions, get_desire, get_desires
+end
