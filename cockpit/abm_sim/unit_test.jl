e81dfaa2fec164837f4547e8504aa5d16033df13
stef <stef@happonomy.org>
2021-01-26 15:39:10 +0100

    Updated

--- cockpit/abm_sim/unit_test.jl
+++ cockpit/abm_sim/unit_test.jl
@@ -5,14 +5,16 @@ using Main.Finance
 using Main.ABM_Sim
 using Agents
 
-@testset "Model" begin
-    properties = Dict(:SuMSy => SuMSy(2000, 25000, 0.02, 30, seed = 5000),
+#@testset "Model" begin
+    prices = Dict{Blueprint, BigFloat}()
+    properties = Dict(:SuMSy => SuMSy(2000, 25000, 0.1, 30, seed = 5000),
+                    :prices => prices,
                     :step => -1)
     model = ABM(Person, properties = properties)
 
-    for n in 1:100
+    for n in 1:20
         add_agent!(model)
     end
 
-    step!(model, agent_step!, model_step!, 100, false)
-end
+    step!(model, agent_step!, model_step!, 100000, false)
+#end
