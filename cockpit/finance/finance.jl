0eaf110d22d8087e0320f9bf3a3f2140b4db82f9
stef <stef@happonomy.org>
2021-01-27 14:26:01 +0100

    Reformatted

--- cockpit/finance/finance.jl
+++ cockpit/finance/finance.jl
@@ -1,8 +1,9 @@
 module Finance
 
 include("balance.jl")
-export EntryType, BalanceEntry, Balance, EQUITY
-export validate, asset, liability, assets, liabilities, asset_value, assets_value, liability_value, liabilities_value, liabilities_net_value, equity
+export EntryType, BalanceEntry, Balance
+export EQUITY, asset, liability
+export validate, assets, liabilities, asset_value, assets_value, liability_value, liabilities_value, liabilities_net_value, equity
 export book_asset!, book_liability!, transfer!, transfer_asset!, transfer_liability!
 
 include("sumsy.jl")
