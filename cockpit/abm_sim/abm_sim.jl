a87f4f2d802763c01de6ec4ea5615dadad5547b9
stef <stef@happonomy.org>
2021-01-28 11:59:47 +0100

    Updated exports

--- cockpit/abm_sim/abm_sim.jl
+++ cockpit/abm_sim/abm_sim.jl
@@ -1,6 +1,7 @@
 module ABM_Sim
     include("model.jl")
-    export Person
-    export get_production, get_needs, get_posessions, get_desire, get_desires
+    export Actor
+    export FUNCTIONS, PERSON
+    export get_production, get_needs, get_posessions, get_desire, get_desires, produce!
     export agent_step!, model_step!
 end
