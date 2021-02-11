4c535e854879b50e6d895e1c54378778f84ec5f7
stef <stef@happonomy.org>
2021-02-11 09:42:46 +0100

    Added stock.jl

--- src/production/production.jl
+++ src/production/production.jl
@@ -13,6 +13,10 @@ include("entities.jl")
 export Entities, Entity
 export num_entities
 
+include("stock.jl")
+export Stock
+export current_stock, stocked, overstocked, add_stock!, retrieve_stock!, min_stock, min_stock!, max_stock, max_stock!, stock_limits, stock_limits!
+
 include("products.jl")
 export Enhancer, Consumable, Product, Producer
 export id, get_blueprint, is_type, produce!
