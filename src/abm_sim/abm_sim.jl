d267b49b7bbbe499cd860b4d5aba223d31a2c74c
stef <stef@happonomy.org>
2021-02-11 09:41:49 +0100

    Removed stock.jl (moved to Production module)

--- src/abm_sim/abm_sim.jl
+++ src/abm_sim/abm_sim.jl
@@ -3,7 +3,7 @@ module ABM_Sim
     export Marginality, process
 
     include("needs.jl")
-    export Needs
+    export Needs, Need
     export want, usage
     export push_usage!, push_want!, delete_usage!, delete_want!
     export is_prioritised, usage_prioritised, wants_prioritised
@@ -11,5 +11,10 @@ module ABM_Sim
     include("actor.jl")
     export Actor
     export push_producer!, delete_producer!, produce!, purchase!
-    export get_posessions, get_production_output, process_needs, process_usage, process_wants
+    export get_posessions, get_production_output
+    export actor_step!
+
+    include("marginal_actor.jl")
+    export marginal_actor
+    export process_needs, process_usage, process_wants
 end
