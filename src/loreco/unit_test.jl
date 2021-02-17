cd9a884c9d24c530bdbe0a8ea6772529dc2c74ec
stef <stef@happonomy.org>
2021-02-17 11:40:44 +0100

    Unite test is example code.

--- src/loreco/unit_test.jl
+++ src/loreco/unit_test.jl
@@ -8,6 +8,29 @@ using ..Econo_Sim
 using ..Loreco
 
 @testset "Loreco model" begin
+    # Create the Loreco model.
     model = init_loreco_model()
-    # step!(model, actor_step!, loreco_model_step!, 100000, false)
+
+    # Execute a default step
+    econo_step!(model, 60)
+
+    sumsy_data = Dict{Symbol, Float64}(CONSUMER => 0, BAKER => 0, TV_MERCHANT => 0, GOVERNANCE => 0)
+
+    for actor in allagents(model)
+        if has_type(actor, CONSUMER)
+            symbol = CONSUMER
+        elseif has_type(actor, BAKER)
+            symbol = BAKER
+        elseif has_type(actor, TV_MERCHANT)
+            symbol = TV_MERCHANT
+        elseif has_type(actor, GOVERNANCE)
+            symbol = GOVERNANCE
+        end
+
+        sumsy_data[symbol] = sumsy_data[symbol] + sumsy_balance(actor)
+    end
+
+    sumsy_data[CONSUMER] = round(sumsy_data[CONSUMER] / 380, digits = 2)
+    sumsy_data[BAKER] = round(sumsy_data[BAKER] / 15, digits = 2)
+    sumsy_data[TV_MERCHANT] = round(sumsy_data[TV_MERCHANT] / 20, digits = 2)
 end
